e4bcc7ef5ffc1793f02e7f065df1e9e02f05d470
WhatisC0ding <92692520+WhatisC0ding@users.noreply.github.com>
2021-11-17 11:10:11 +0100

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,10 @@
+credentials = {
+    'ssid' : 'Magnus',
+    'password' : 'limkarsen',
+    'ADAFRUIT_IO_URL' : b'io.adafruit.com',
+    'ADAFRUIT_USERNAME' : b'DannyLy',
+    'ADAFRUIT_IO_KEY' : b'aio_gVLy82zO8iXVEplUc9gLEJbcdQ7e',
+    'ADAFRUIT_IO_PUB_FEEDNAME' : b'bot_pub',
+    'ADAFRUIT_IO_SUB_FEEDNAME' : b'bot_sub'
+    }
+
